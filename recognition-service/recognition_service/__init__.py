88d99eec070bd5e19a9a32cc9719e8e2a6caefd5
Matt Kirby <MattJKirby@outlook.com>
2022-10-17 00:27:09 +0100

    Basic image test route

--- recognition-service/recognition_service/__init__.py
+++ recognition-service/recognition_service/__init__.py
@@ -1,12 +1,42 @@
-from flask import Flask
+import base64
+import sys
+from PIL import Image
+import numpy as np
+from recognition_service.imageProcessor import ImageProcessor
+
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config.from_object("recognition_service.configuration.Config")
+# db = SQLAlchemy(app)
+
+# class PetProfile(db.Model):
+#     __tablename__ = 'Pet Profiles'
+    
+#     id = db.Column(db.Integer, primary_key=True)
+#     pet_name = db.Column(db.String(128), unique=False, nullable=False)
+
+
+
 
 
 @app.route('/')
 def hello():
+    print("Hello", file=sys.stderr)
     return 'Hello, World!'
 
-@app.route('/test')
-def test():
+@app.route('/image', methods=['POST'])
+def image():
+  if 'image' not in request.files:
+    return 'No files received!'
+
+  file = request.files['image']
+  image_processor = ImageProcessor()
+  processed_image = image_processor.pre_process_image(file)
+
+  print(processed_image, file=sys.stderr)
+
+  
   return 'This is a test route!'
