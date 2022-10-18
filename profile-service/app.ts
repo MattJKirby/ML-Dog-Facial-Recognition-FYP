b003ade7611b433f6e36e1668ec184682bc8674a
Matt Kirby <MattJKirby@outlook.com>
2022-10-18 01:21:50 +0100

    Now using mongo connection provider

--- profile-service/app.ts
+++ profile-service/app.ts
@@ -1,12 +1,16 @@
 import express, { Application } from 'express'
 import { initializeRoutes } from './src/Startup/routes'
-import multer from 'multer'
+import { MongoConnectionProvider } from './src/Database/Utility/MongoConnectionProvider'
 
 // Server configuration
 const server:Application = express()
 
 // InitaliseRoutes
 initializeRoutes(server)
+
+// Database configuration
+export const MongoClient = new MongoConnectionProvider('db','localhost', 27017)
+MongoClient.Connect()
 
 
 server.listen(5002, ():void => {
