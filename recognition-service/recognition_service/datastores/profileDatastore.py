6e935ea15e1d9cf2b832aa7af6ed056d029b798c
Matt Kirby <MattJKirby@outlook.com>
2022-11-04 18:29:07 +0000

    Gets all profiles from profile service

--- recognition-service/recognition_service/datastores/profileDatastore.py
+++ recognition-service/recognition_service/datastores/profileDatastore.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 import io
 from PIL import Image
 from recognition_service.imageProcessor import ImageProcessor
@@ -9,13 +10,25 @@ class ProfileDatastore:
     self.database = database
     self.collection = 'profiles'
 
-  def save_profile(self, name, user, img_arr):
-    images = []
-    for image in img_arr:
-      img = Image.open(image)
-      image_bytes = io.BytesIO()
-      img.save(image_bytes, format='PNG')
-      images.append(image_bytes.getvalue())
+  # def save_profile(self, name, user, img_arr):
+  #   images = []
+  #   for image in img_arr:
+  #     img = Image.open(image)
+  #     image_bytes = io.BytesIO()
+  #     img.save(image_bytes, format='PNG')
+  #     images.append(image_bytes.getvalue())
 
 
-    self.database[self.collection].insert_one({'name': name, 'images': images})
+  #   self.database[self.collection].insert_one({'name': name, 'images': images})
+
+  def getAllProfiles(self):
+    profileList = list(self.database[self.collection].find({}, {'_id': 0, 'ProfileUid': 1 ,'Images.filePath': 1}))
+    profiles = {}
+
+    for profile in profileList:
+      imagePathList = []
+      for image in profile['Images']:
+        imagePathList.append(image['filePath'])
+      profiles[profile['ProfileUid']] = imagePathList
+
+    return profiles
