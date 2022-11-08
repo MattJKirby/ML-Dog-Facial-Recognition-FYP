6b01819c01054289777b4dba472cb464fdbac248
Matt Kirby <MattJKirby@outlook.com>
2022-11-08 23:02:56 +0000

    Updated Field names in datastore

--- recognition-service/recognition_service/datastores/profileDatastore.py
+++ recognition-service/recognition_service/datastores/profileDatastore.py
@@ -10,32 +10,22 @@ class ProfileDatastore:
     self.database = database
     self.collection = 'profiles'
 
-  # def save_profile(self, name, user, img_arr):
-  #   images = []
-  #   for image in img_arr:
-  #     img = Image.open(image)
-  #     image_bytes = io.BytesIO()
-  #     img.save(image_bytes, format='PNG')
-  #     images.append(image_bytes.getvalue())
-
-
-  #   self.database[self.collection].insert_one({'name': name, 'images': images})
 
   def getProfileImagesByUid(self, profileUid):
     profile = self.database[self.collection].find_one({'ProfileUid': profileUid})
     imageList = []
     for image in profile['Images']:
-      imageList.append(image['filePath'])
+      imageList.append(image['FilePath'])
     return imageList
 
   def getAllProfiles(self):
-    profileList = list(self.database[self.collection].find({}, {'_id': 0, 'ProfileUid': 1 ,'Images.filePath': 1}))
+    profileList = list(self.database[self.collection].find({}, {'_id': 0, 'ProfileUid': 1 ,'Images.FilePath': 1}))
     profiles = {}
 
     for profile in profileList:
       imagePathList = []
       for image in profile['Images']:
-        imagePathList.append(image['filePath'])
+        imagePathList.append(image['FilePath'])
       profiles[profile['ProfileUid']] = imagePathList
 
     return profiles
