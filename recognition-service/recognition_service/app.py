12b679f214507c8f57211628ae2be5735edf7af4
Matt Kirby <MattJKirby@outlook.com>
2022-11-08 23:02:14 +0000

    Cleaned app.py

--- recognition-service/recognition_service/app.py
+++ recognition-service/recognition_service/app.py
@@ -17,11 +17,7 @@ app = Flask(__name__)
 db_connector = MongoConnector('localhost',27017)
 profileManager = ProfileManager(db_connector)
 recog = RecognitionService('./model/dog_face_model_00888_val_loss_weights.hdf5')
-
-print(profileManager.loadProfiles()[1].shape)
-
-print(profileManager.newProfile('52ec5fe3-437d-4bab-9a28-274790dd14ea')[1].shape)
-
+profileManager.loadProfiles()
 
 
 @app.route('/')
@@ -34,8 +30,7 @@ def hello():
 @app.route('/loadProfile', methods=['POST'])
 def newProfile():
   profileUid = request.form.get('ProfileUid')
-  profileData = profileManager.newProfile(profileUid)
-  print(profileData)
+  profileManager.newProfile(profileUid)
 
   return 'This is a test route!'
 
