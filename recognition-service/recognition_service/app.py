108e01ba8a680fd83c9fdf53672579d35a875c26
Matt Kirby <MattJKirby@outlook.com>
2022-11-09 00:44:59 +0000

    Knn can now fit loaded profiles

--- recognition-service/recognition_service/app.py
+++ recognition-service/recognition_service/app.py
@@ -8,6 +8,7 @@ import numpy as np
 from recognition_service.imageProcessor import ImageProcessor
 from recognition_service.mongoConnector import MongoConnector
 from recognition_service.datastores.profileDatastore import ProfileDatastore
+from recognition_service.recognition.nearestNeighbourClassifier import NearestNeighbourClassifier
 
 
 from flask import Flask, request, jsonify
@@ -17,7 +18,11 @@ app = Flask(__name__)
 db_connector = MongoConnector('localhost',27017)
 profileManager = ProfileManager(db_connector)
 recog = RecognitionService('./model/dog_face_model_00888_val_loss_weights.hdf5')
-profileManager.loadProfiles()
+knn = NearestNeighbourClassifier()
+profileData = profileManager.loadProfiles()
+
+knn.fitData(recog.generate_image_embeddings(profileData[1]), profileData[0])
+
 
 
 @app.route('/')
@@ -30,7 +35,8 @@ def hello():
 @app.route('/loadProfile', methods=['POST'])
 def newProfile():
   profileUid = request.form.get('ProfileUid')
-  profileManager.newProfile(profileUid)
+  profileData = profileManager.newProfile(profileUid)
+  knn.fitData(recog.generate_image_embeddings(profileData[1]), profileData[0])
 
   return 'This is a test route!'
 
