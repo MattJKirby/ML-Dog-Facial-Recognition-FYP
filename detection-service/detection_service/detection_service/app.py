a3053883f2b66c57010b87cbbade357afc093d48
Matt Kirby <MattJKirby@outlook.com>
2022-11-21 02:09:31 +0000

    Return json with confidence score and image

--- detection-service/detection_service/detection_service/app.py
+++ detection-service/detection_service/detection_service/app.py
@@ -1,15 +1,17 @@
 import sys
 from io import BufferedReader
 from detection_service.model.detectionModel import DetectionModel
+from detection_service.imageProcessing.imageProcessor import ImageProcessor
 from PIL import Image
 import io
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import torch
 
 app = Flask(__name__)
 
 
 detectionModel = DetectionModel('./model/Tsinghua_train3_best.pt')
+imageProcessor = ImageProcessor(minOutputResolution=00)
 
 
 @app.route('/')
@@ -21,5 +23,17 @@ def hello():
 def predict():
   file = request.files['image']
   image = file.read()
-  print(detectionModel.predict(image))
-  return ""
+  bbox_parameters, prediction = detectionModel.predict(image)
+
+  
+  try:
+    if prediction['confidence'] < 0.85:
+      raise Exception("Detection does not match confidence constraints.")
+
+    return {
+      "confidence": prediction['confidence'],
+      "detection": imageProcessor.encode_pil_image(imageProcessor.isolateDetection(image, bbox_parameters))
+     }
+  except Exception as e:
+    print(e)
+    return {'error': 'Error detecting dog face. Please upload a different image.'}
