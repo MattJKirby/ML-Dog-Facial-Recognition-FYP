d29477c7fb6470e9f6347cc6686aa53670c3773c
Matt Kirby <MattJKirby@outlook.com>
2023-02-02 21:09:40 +0000

    Added service status component

---
+++ frontend-app/src/components/ServiceStatusItem/index.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Text } from "grommet";
+import { Refresh } from "grommet-icons";
+import { FC, PropsWithChildren, useEffect, useState } from "react";
+
+type ServiceStatusItemProps ={
+  serviceName: string;
+  serviceEndpoint: string;
+}
+
+type ServiceStatus = 'Connected' | 'Waiting' | 'Disconnected'
+
+
+export const ServiceStatusItem: FC<PropsWithChildren<ServiceStatusItemProps>> = ({
+  children,
+  serviceName,
+  serviceEndpoint
+}) => {
+  const [status, setStatus] = useState<ServiceStatus>('Disconnected');
+  const [msg, setMsg] = useState<string>('');
+
+
+  const getStatus = async () => {
+    setStatus('Waiting');
+    const statusResult = await fetch(serviceEndpoint);
+    setMsg(statusResult[1].data.body);
+
+    if(statusResult[0]){
+      setStatus('Connected');
+    } else {
+      setStatus('Disconnected');
+    }
+  }
+
+  return (
+    <Box style={{whiteSpace: 'nowrap'}} direction="row" width={"100%"} align='center' justify="between">
+      <Text size='small'>{serviceName}</Text>
+      <Box direction="row" align='center'>
+        <Button size='small' icon={<Refresh size='small'/>} onClick={() => getStatus()}/>
+        <Text size='xsmall'>{!status ? '🔴' : '🟢'}</Text>
+      </Box> 
+    </Box>
+  )
+}
+
+const fetch = async (url: string): Promise<[boolean, any]> => {
+
+  try{
+    const res = fetch(url)
+    return [true, await res.data];
+
+  } catch (err) {
+    return [false, err];
+  }
+  
+}
+
