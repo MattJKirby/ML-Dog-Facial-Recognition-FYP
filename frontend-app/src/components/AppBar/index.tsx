ff8d088337afddd2379a4b28b712d5d81f9d22df
Matt Kirby <MattJKirby@outlook.com>
2023-02-02 21:10:15 +0000

    Modified header stylings

--- frontend-app/src/components/AppBar/index.tsx
+++ frontend-app/src/components/AppBar/index.tsx
@@ -1,26 +1,31 @@
 import { Anchor, Box, Button, Header, HeaderProps, Text } from "grommet";
 import { Add, Home, Menu } from 'grommet-icons'
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useState } from "react";
+import { LayerMenu } from "../LayerMenu";
 
 type AppBarProps ={
   props?: HeaderProps;
 }
 
 export const AppBar: FC<PropsWithChildren<AppBarProps>> = ({children, props}) => {
+  const [menu, setMenu] = useState(false);
+  
+  
   return (
     <div style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
-      <Header background="brand"
+      <Header background="#FFF"
       flex={'grow'}
         pad={{ left: "small", right: "small", vertical: "small" }}
-        elevation="small"
+        elevation="none"
         {...props}>
           <div>
-            <Button primary  icon= {<Menu />} label="Menu"/>
+            <Button  icon= {<Menu />} label="Menu" onClick={() => setMenu(!menu)}/>
             <Anchor icon={<Home />} size='xsmall' />
           </div>
-          <Text size="large">Dog Search App</Text>
+          <Text size="large"></Text>
           {children}
       </Header>
+      <LayerMenu display={menu} setDisplay={() => setMenu(!menu)} />
     </div>
   )
 }
