52c945cf8eb0fc8be1e1375acde9093b20a90bc1
Matt Kirby <MattJKirby@outlook.com>
2023-02-04 18:29:15 +0000

    Added cors configuration to frontend

--- frontend-app/next.config.js
+++ frontend-app/next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/detection/:path*',
+        destination: 'http://localhost:5000/:path*'
+      }
+    ]
+  }
+}
+
 module.exports = nextConfig
+
+
