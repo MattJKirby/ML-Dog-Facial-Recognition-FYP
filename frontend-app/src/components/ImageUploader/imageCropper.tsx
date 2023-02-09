0b505be832bae8f94e8d0b105fc6d8bcc759c7da
Matt Kirby <MattJKirby@outlook.com>
2023-02-09 20:17:30 +0000

    Now using crop control

--- frontend-app/src/components/ImageUploader/imageCropper.tsx
+++ frontend-app/src/components/ImageUploader/imageCropper.tsx
@@ -1,4 +1,7 @@
-import { FC, PropsWithChildren } from "react"
+import { Box, Button, Image } from "grommet";
+import { Edit } from "grommet-icons";
+import { FC, PropsWithChildren, useState } from "react"
+import { CropPanel } from "./cropPanel";
 
 type ImageCropperProps = {
   results: any[];
@@ -7,11 +10,45 @@ type ImageCropperProps = {
 
 
 export const ImageCropper: FC<PropsWithChildren<ImageCropperProps>> = ({results, images}) => {
-  {console.log(results, images)}
+  const [croppedOutputs, setCroppedOutputs] = useState<Map<string, string>>(new Map())
+  const [edit, setEdit] = useState<string | null>(null);
+  
+  const handleCropUpdate = (name: string, output:string) => {
+    if(output !== croppedOutputs.get(name)){
+      setCroppedOutputs(prev => new Map(prev.set(name, output)))
+    }
+  }
+
 
   return (
-    <>
+    <Box>
+      {images && images.map((img) => {
+        const result = results.find((r) => r.name === img.name);
+        if(result){
+          return <CropPanel 
+            key={img.name} 
+            bbox={result.boundingBoxCoordinates} 
+            display={img.name === edit} 
+            image={img} 
+            onOutputChange={(name, output) => handleCropUpdate(name, output)}
+            onAccept={() => (setEdit(null))}
+            />
+        }  
+      }
+      )}
 
-    </>
+      <Box direction="row" >
+        {Array.from(croppedOutputs.entries()).map(output => {
+          return (
+            <Box key={output[0]} style={{width: "200px"}} margin='small' align="center">
+              <Box style={{width: '200px', height: '200px', boxSizing: 'content-box'}} background='bg2'>
+                <Image alt={output[0]} style={{objectFit: 'contain'}} width={200} height={200}  src={output[1]}></Image>
+              </Box>
+              <Button size="small" onClick={() => setEdit(output[0])} label="Edit" margin={'small'}/>
+            </Box>
+            )
+        })}
+      </Box>
+    </Box>
   )
 }
