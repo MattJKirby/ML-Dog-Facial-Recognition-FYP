64f61e3677cc578bef7a889eed4db44fc33c630b
Matt Kirby <MattJKirby@outlook.com>
2023-02-14 15:49:21 +0000

    Added cors to recognition service

--- recognition-service/recognition_service/app.py
+++ recognition-service/recognition_service/app.py
@@ -9,12 +9,24 @@ from recognition_service.imageProcessor import ImageProcessor
 from recognition_service.mongoConnector import MongoConnector
 from recognition_service.datastores.profileDatastore import ProfileDatastore
 from recognition_service.recognition.nearestNeighbourClassifier import NearestNeighbourClassifier
-
-
 from flask import Flask, request, jsonify
 
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+if __name__ == "__main__":
+  app.run(host="localhost", port=8000, debug=True)
+
+
+CORS(app)
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"*"
+    }
+})
+
+
 db_connector = MongoConnector('localhost',27017)
 profileManager = ProfileManager(db_connector)
 recog = RecognitionService('./model/dog_face_model_00888_val_loss_weights.hdf5')
@@ -25,12 +37,9 @@ knn.fitData(recog.generate_image_embeddings(profileData[1]), profileData[0])
 
 
 
-@app.route('/')
-def hello():
-    print("Hello", file=sys.stderr)
-    db = db_connector.connect('db')
-    db['test_collection'].insert_one({'test': 1234})
-    return 'Hello, World!'
+@app.route('/status')
+def statusCheck():
+  return  {'message': 'Recognition service is running', 'statusCode': 200}
 
 @app.route('/loadProfile', methods=['POST'])
 def newProfile():
