ff4ef17cfe159b13333c12fe5fa7e727122ff226
Matt Kirby <MattJKirby@outlook.com>
2023-02-19 02:25:02 +0000

    Basic Profile search page

--- frontend-app/pages/search/index.tsx
+++ frontend-app/pages/search/index.tsx
@@ -6,12 +6,23 @@ import { Anchor, Box, Button, Grommet, Page, PageContent, PageHeader } from "gro
 import Router from "next/router";
 import { useState } from "react";
 
+type DogProfile = {
+  ProfileUid: string;
+  PetName: string;
+  PetBreed: string;
+  OwnerMobileNumber: string;
+  OwnerFirstName: string;
+  OwnerLastName: string;
+  Images: any[];
+}
+
 const Search = () => {
   const [uploader, setUploader] = useState<boolean>(true);
   const [cropper, setCropper] = useState<boolean>(false);
   const [detectionResults, setDetectionResults] = useState<DetectionResults[] | null>(null);
   const [sourceImages, setSourceImages] = useState<File[]>([]);
   const [croppedImages, setCroppedImages] = useState<File[]>([]);
+  const [matchedProfiles, setMatchedProfiles] = useState<DogProfile[] | null>(null)
   
   const onValidUpload = (results: any, images: File[]) => {
     setUploader(false);
@@ -30,7 +41,11 @@ const Search = () => {
   }
 
   const handleSubmit = () => {
-    predictApiCall('http://127.0.0.1:5001/predict',croppedImages[0])
+    predictApiCall('http://127.0.0.1:5001/predict',croppedImages[0]).then((res) => {
+      setMatchedProfiles(res)
+      console.log(res)
+    })
+    
   }
 
   return(
@@ -40,24 +55,47 @@ const Search = () => {
         <PageContent justify='center'>
           <Box fill={true} justify='center' width={'800px'} align='center'>
             <Box style={{maxWidth: '800px', width: "100%"}}>
-              <PageHeader
-                title="Identify a Dog"
-                subtitle="Upload an image to identify a missing dog."
-                parent={<Anchor label="Search" onClick={() => Router.reload()}/>}
-                actions={ 
-                  <Box direction="row" gap="medium">
-                    <Button primary label="Search" disabled={croppedImages.length !== 1} onClick={() => handleSubmit()}/>
-                    <Button type="reset" label="Reset" onClick={() => Router.reload()} />
-                  </Box>}
-              />
-              {uploader && 
-                <ImageUploader min={1} max={1} onValidUpload={(results:DetectionResults[], images: File[]) => onValidUpload(results, images)}/>
-              }
+            {!matchedProfiles ? 
+              <Box>
+                <PageHeader
+                  title="Identify a Dog"
+                  subtitle="Upload an image to identify a missing dog."
+                  parent={<Anchor label="Search" onClick={() => Router.reload()}/>}
+                  actions={ 
+                    <Box direction="row" gap="medium">
+                      <Button primary label="Search" disabled={croppedImages.length !== 1} onClick={() => handleSubmit()}/>
+                      <Button type="reset" label="Reset" onClick={() => Router.reload()} />
+                    </Box>}
+                />
+                {uploader && 
+                  <ImageUploader min={1} max={1} onValidUpload={(results:DetectionResults[], images: File[]) => onValidUpload(results, images)}/>
+                }
 
-              {cropper && detectionResults &&
-                <ImageCropper results={detectionResults} images={sourceImages} updateOutput={handleCropUpdate}/>
-              }
-           </Box>  
+                {cropper && detectionResults &&
+                  <ImageCropper results={detectionResults} images={sourceImages} updateOutput={handleCropUpdate}/>
+                }
+              </Box>
+            : 
+              <Box>
+                <PageHeader
+                  title="Search Results:"
+                  subtitle="Upload an image to identify a missing dog."
+                  parent={<Anchor label="Search" onClick={() => Router.reload()}/>}
+                  actions={ 
+                    <Box direction="row" gap="medium">
+                      <Button type="reset" label="Try another search" onClick={() => Router.reload()} />
+                    </Box>}
+                />
+                {matchedProfiles.map(profile => {
+                  return (
+                  <Box key={profile.ProfileUid}>
+                    {profile.PetName}
+                  </Box>
+                  )
+                })}
+              </Box>
+            }
+           </Box> 
           </Box>  
        </PageContent>  
       </Page>
@@ -69,7 +107,7 @@ export default Search;
 
 const predictApiCall = async (url: string, target: File) => {
   const formData = new FormData();
-  formData.append(`image`, target);
+  formData.append(`target`, target);
 
   const reqOptions = {
     method: 'POST',
