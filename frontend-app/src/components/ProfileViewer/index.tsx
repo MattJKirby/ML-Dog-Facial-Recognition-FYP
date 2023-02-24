a1ff6c6faa0ff35da4ea95d4efcb609449935205
Matt Kirby <MattJKirby@outlook.com>
2023-02-24 02:26:25 +0000

    Added new profile viewer component

---
+++ frontend-app/src/components/ProfileViewer/index.tsx
@@ -0,0 +1,28 @@
+import { DogProfile } from "@/pages/search";
+import { Box, Text } from "grommet";
+import Image from "next/image";
+import { FC, PropsWithChildren, useState } from "react";
+
+type ProfileViewerProps = {
+  profile: DogProfile;
+  size?: number;
+}
+
+const ProfileViewer:FC<PropsWithChildren<ProfileViewerProps>> = ({profile, size}) => {
+  const images = profile.Images.map(img => {
+    return img.FilePath.split('public')[1]
+  })
+
+  console.log(images)
+
+  return (
+    <Box direction="row" justify="evenly" fill="horizontal" round>
+      {images.map(img => {
+        return <img key={img} width={size || 150} height={size || 150} alt={'Dog main image'} src={`http://localhost:5002${img}`}/>
+
+      })}
+    </Box>
+  )
+}
+
+export default ProfileViewer;
