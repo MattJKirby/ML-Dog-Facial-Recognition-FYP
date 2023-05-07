def0764abf8caa3d1a285f143a7e09f920d22707
Matt Kirby <MattJKirby@outlook.com>
2023-05-07 15:59:27 +0100

    Added frontend Dockerfile

--- frontend-app/next.config.js
+++ frontend-app/next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
 module.exports = {
   async rewrites() {
     return [
       {
-        source: '/detection/:path*',
-        destination: 'http://localhost:5000/:path*'
-      }
-    ]
-  }
-}
+        source: "/detection/:path*",
+        destination: "http://localhost:8080/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
-
-
+module.exports = nextConfig;
