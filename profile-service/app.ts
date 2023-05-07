e04ccf141349fa2b65dacdb86d3858dd964c70e6
Matt Kirby <MattJKirby@outlook.com>
2023-05-07 16:00:26 +0100

    Fixed app public static route

--- profile-service/app.ts
+++ profile-service/app.ts
@@ -1,28 +1,27 @@
-import express, { Application } from 'express'
-import { initializeRoutes } from './src/Startup/routes'
-import { MongoConnectionProvider } from './src/Database/Utility/MongoConnectionProvider'
-import cors from 'cors';
+import express, { Application } from "express";
+import { initializeRoutes } from "./src/Startup/routes";
+import { MongoConnectionProvider } from "./src/Database/Utility/MongoConnectionProvider";
+import cors from "cors";
 
 // Server configuration
-const server:Application = express();
+const server: Application = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ["http://localhost:3000"],
 };
 
-server.use(cors(corsOptions))
+server.use(cors(corsOptions));
 
 // Configure static resources
-server.use(express.static(__dirname + '/public'));
+server.use(express.static("/app" + "/public"));
 
 // InitaliseRoutes
-initializeRoutes(server)
+initializeRoutes(server);
 
 // Database configuration
-export const MongoClient = new MongoConnectionProvider('db','localhost', 27017)
-MongoClient.Connect()
+export const MongoClient = new MongoConnectionProvider("db", "mongodb", 27017);
+MongoClient.Connect();
 
-
-server.listen(5002, ():void => {
-  console.log('Service: Running here 👉 https://localhost:5002')
-})
+server.listen(3000, (): void => {
+  console.log("Service: Running here 👉 https://localhost:3000");
+});
